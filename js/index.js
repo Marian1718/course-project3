2b0aa40e11bd6df479f0f6251bb593f89cbde8a6
marian vintsiv <vintsiv.mark.com@gmail.com>
2019-05-15 09:40:47 +0000

    8

--- js/index.js
+++ js/index.js
@@ -15,3 +15,19 @@ function currConvert(e) {
             .innerText = convertedAmount.toFixed(2);
         });
 }
+
+$(function() {
+  $('.smooth').on('click', function(event) {
+    const target = $(this.getAttribute('href'));
+    if (target.length) {
+      event.preventDefault();
+      $('html, body')
+        .stop()
+        .animate(
+          {
+            scrollTop: target.offset().top
+          },
+          1000
+        );
+    }
+  });
